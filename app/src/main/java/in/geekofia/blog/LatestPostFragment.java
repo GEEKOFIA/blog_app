c0f3e13d22cdc7791b5c2e7752c464c06052356a
chankruze <chankruze@gmail.com>
2019-06-13 06:54:46 +0530

    Using API v1 To Fetch Posts
    * unused resources cleanup
    * add blank fragments for future
    * update colors
    * add search item to toolbar (just ui, non functional for now) and remove review item
    
    Signed-off-by: chankruze <chankruze@gmail.com>

---
+++ app/src/main/java/in/geekofia/blog/LatestPostFragment.java
@@ -0,0 +1,23 @@
+package in.geekofia.blog;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class LatestPostFragment extends Fragment {
+    View view;
+
+    public LatestPostFragment() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_latest_posts, container, false);
+        return view;
+    }
+}
--- app/src/main/java/in/geekofia/blog/MainActivity.java
+++ app/src/main/java/in/geekofia/blog/MainActivity.java
@@ -1,6 +1,9 @@
 package in.geekofia.blog;
 
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
@@ -8,15 +11,35 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
 import java.util.ArrayList;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
     private Toolbar mTopToolbar;
     private PostAdapter mAdapter;
+    private ListView mLatestPostsListView;
+    private Button mRetryButton;
+    private ProgressBar mLoadingIndicator;
+    private TextView mEmptyStateTextView;
+
+    private static final String GEEKOFIA_BLOG_ENDPOINT = "https://blog.geekofia.in/api/v1";
+    private static final String LOG_TAG = MainActivity.class.getName();
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -43,18 +66,98 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        BottomNavigationView navigation = findViewById(R.id.navigation);
+        // Initialize UI Elements
+        initializeViews();
+
+        // Send HTTP Request & Get Latest Posts
+        startLoader();
+    }
+
+    private void initializeViews() {
+        // Bottom Navigation Bar
+        BottomNavigationView navigation = findViewById(R.id.id_BottomNavigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
-        mTopToolbar = findViewById(R.id.toolbar);
+        // Toolbar
+        mTopToolbar = findViewById(R.id.id_Toolbar);
         setSupportActionBar(mTopToolbar);
 
-        ListView latestPostListView = findViewById(R.id.list);
+        // Latest Posts List
+        mLatestPostsListView = findViewById(R.id.id_LatestPostsList);
 
-        // Create a fake list of earthquake locations.
-        final ArrayList<Post> posts = PostUtils.extractPosts();
-        mAdapter = new PostAdapter(this, posts);
-        latestPostListView.setAdapter(mAdapter);
+        // Loading Indicator
+        mLoadingIndicator = findViewById(R.id.id_LoadingIndicator);
+
+        mEmptyStateTextView = findViewById(R.id.id_EmptyView);
+
+        // Retry Button
+        mRetryButton = findViewById(R.id.id_RetryButton);
+        mRetryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mEmptyStateTextView.setText("");
+                mRetryButton.setVisibility(View.GONE);
+                mLoadingIndicator.setVisibility(View.VISIBLE);
+                mRetryButton.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        startLoader();
+                    }
+                }, 1000);
+            }
+        });
+
+    }
+
+    private void startLoader() {
+        if (isConnected()) {
+            mLoadingIndicator.setVisibility(View.VISIBLE);
+            syncLatestPosts();
+        } else {
+            mLoadingIndicator.setVisibility(View.GONE);
+            mEmptyStateTextView.setText(R.string.no_internet_connection);
+            mRetryButton.setVisibility(View.VISIBLE);
+        }
+    }
+
+    private void syncLatestPosts() {
+        OkHttpClient client = new OkHttpClient();
+
+        final Request request = new Request.Builder()
+                .url(GEEKOFIA_BLOG_ENDPOINT)
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NotNull Call call, @NotNull IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+                if (response.isSuccessful()) {
+                    final String myResponse;
+                    myResponse = response.body().string();
+
+                    MainActivity.this.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            ArrayList<Post> posts = PostUtils.extractPosts(myResponse);
+                            mAdapter = new PostAdapter(MainActivity.this, posts);
+                            mLoadingIndicator.setVisibility(View.GONE);
+                            mLatestPostsListView.setAdapter(mAdapter);
+                        }
+                    });
+                }
+            }
+        });
+    }
+
+    private boolean isConnected() {
+        // Check for connectivity status
+        ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetwork = connMgr.getActiveNetworkInfo();
+        return activeNetwork != null && activeNetwork.isConnectedOrConnecting();
     }
 
     @Override
