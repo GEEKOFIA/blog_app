663a751ab465ce5933651aab20bc38e349b27735
chankruze <chankruze@gmail.com>
2019-08-11 19:29:31 +0530

    Complete Reading Layout

--- app/src/main/java/in/geekofia/blog/activities/ReadingActivity.java
+++ app/src/main/java/in/geekofia/blog/activities/ReadingActivity.java
@@ -7,6 +7,9 @@ import android.os.Bundle;
 import android.text.Html;
 import android.util.Log;
 import android.view.View;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -29,6 +32,7 @@ public class ReadingActivity extends AppCompatActivity {
     private ImageView mPostFeaturedImageView;
     private String mPostURL, mPostFeaturedImageURL, mPostTitle, mPostDate, mPostAuthor, mPostDuration;
     private ShimmerFrameLayout mShimmerViewContainer;
+    private WebView mWebView;
 
     private static final String PROTO_ONE = "https://", PROTO_TWO = "http://", DOMAIN_URL = "https://blog.geekofia.in";
 
@@ -46,37 +50,25 @@ public class ReadingActivity extends AppCompatActivity {
                 try {
                     final Document post;
                     post = Jsoup.connect(mPostURL).get();
+                    post.select("div.post-contents").select("p").attr("style", "color: #000000;");
+                    // Post title
+                    mPostTitle = post.select("h1").text();
+                    // Post metadata
+                    Elements postMetadata = post.select("ul.metadata.top");
+                    // post date
+                    mPostDate = postMetadata.select("li.post-date").text();
+                    // post author
+                    mPostAuthor = postMetadata.select("li.post-author").text();
+                    // post duration
+                    mPostDuration = postMetadata.select("li.post-duration").text().split(" read")[0];
+                    // Image
+                    Elements postFeaturedImage = post.select("img[alt$=featured-image]");
+                    mPostFeaturedImageURL = postFeaturedImage.attr("src");
+                    postFeaturedImage.remove();
+
                     runOnUiThread(new Runnable() {
                         @Override
                         public void run() {
-                            post.select("div.post-contents").select("p").attr("style", "color: #000000;");
-//                            Elements links = post.select("div.post-contents a");
-//                            links.attr("style", "color: #00FF00;");
-//                            post.select("pre").attr("color: #00FF00; background: #000000");
-//                            post.select("div.post-contents ul").attr("style", "list-style: none;");
-//                            post.select("div.post-contents li").attr("style", "color: #0000FF;");
-//                            Elements headings = post.select("h4");
-//                            for (Element h4 : headings){
-//                                h4.attr("style", "color: #FF0000");
-//                                Log.i("TAG", h4.attributes().toString());
-//                            }
-//                            Log.i("TAG",post.select("div.post-contents").select("p").attr("style", "color: #000000;").toString());
-                            // Post title
-                            mPostTitle = post.select("h1").text();
-                            // Post metadata
-                            Elements postMetadata = post.select("ul.metadata.top");
-                            // post date
-                            mPostDate = postMetadata.select("li.post-date").text();
-                            // post author
-                            mPostAuthor = postMetadata.select("li.post-author").text();
-                            // post duration
-                            mPostDuration = postMetadata.select("li.post-duration").text().split(" read")[0];
-                            // Image
-                            Elements postFeaturedImage = post.select("img[alt$=featured-image]");
-                            mPostFeaturedImageURL = postFeaturedImage.attr("src");
-
-                            Elements mPostContents = post.select("div.post-contents");
-
                             mPostTitleView.setText(mPostTitle);
                             mPostDateView.setText(mPostDate);
                             mPostAuthorView.setText(mPostAuthor);
@@ -88,18 +80,47 @@ public class ReadingActivity extends AppCompatActivity {
                                     mPostFeaturedImageURL = DOMAIN_URL + mPostFeaturedImageURL;
                                     Picasso.get().load(mPostFeaturedImageURL).into(mPostFeaturedImageView);
                                 }
-                            }
-                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-                                mPostContentView.setText(Html.fromHtml(mPostContents.html(), Html.FROM_HTML_MODE_COMPACT));
                             } else {
-                                mPostContentView.setText(Html.fromHtml(mPostContents.html()));
+                                mPostFeaturedImageView.setVisibility(View.GONE);
                             }
-                            mPostDateView.setVisibility(View.VISIBLE);
-                            mPostAuthorView.setVisibility(View.VISIBLE);
-                            mPostDurationView.setVisibility(View.VISIBLE);
-                            mShimmerViewContainer.setVisibility(View.GONE);
-                            mShimmerViewContainer.stopShimmer();
+
+                            mWebView.loadUrl(mPostURL);
+                            mWebView.getSettings().setJavaScriptEnabled(true);
+                            mWebView.setWebViewClient(new WebViewClient() {
+
+                                @Override
+                                public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                                    view.loadUrl(url);
+                                    return true;
+                                }
+
+                                @Override
+                                public void onPageFinished(WebView view, String url) {
+                                    mWebView.loadUrl("javascript:(function() { " +
+                                            "let welcome = document.getElementById('welcome'); welcome.classList.remove('d-block');" +
+                                            "welcome.style.display='none';" +
+
+                                            "document.getElementsByClassName('navbar')[0].style.display='none'; " +
+                                            "document.getElementsByTagName('h1')[0].style.display='none'; " +
+
+                                            "document.getElementsByClassName('scrollup')[0].style.display='none'; " +
+                                            "document.getElementsByClassName('footer')[0].style.display='none'; " +
+
+                                            "document.getElementsByClassName('metadata')[0].style.display='none';" +
+                                            "document.getElementsByClassName('metadata')[1].style.display='none';" +
+
+                                            "document.getElementsByClassName('related-posts')[0].style.display='none';" +
+
+                                            "document.getElementsByTagName('img')[0].style.display='none';" +
+                                            "})()");
+
+                                    mShimmerViewContainer.setVisibility(View.GONE);
+                                    mShimmerViewContainer.stopShimmer();
+                                }
+                            });
                         }
+
+
                     });
                 } catch (IOException e) {
                     e.printStackTrace();
@@ -116,7 +137,8 @@ public class ReadingActivity extends AppCompatActivity {
         mPostAuthorView = findViewById(R.id.view_post_author);
         mPostDurationView = findViewById(R.id.view_post_duration);
         mPostFeaturedImageView = findViewById(R.id.view_post_featured_image);
-        mPostContentView = findViewById(R.id.view_post_body);
+//        mPostContentView = findViewById(R.id.view_post_body);
+        mWebView = findViewById(R.id.view_post_body);
     }
 
 }
